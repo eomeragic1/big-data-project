fb42ae57f6b9a235156340c89bb8bae595ea04ee
lukazontar <97096253+lukazontar@users.noreply.github.com>
2023-06-06 18:28:11 +0200

    Update load.py

--- util/etl/load.py
+++ util/etl/load.py
@@ -5,12 +5,12 @@ def load(data: dd.DataFrame,
          table_name: str,
          data_path: str) -> None:
     # Store DataFrame in Parquet format
-    data.head(1000).to_parquet(f'{data_path}/parquet/{table_name}.parquet')
+    data.to_parquet(f'{data_path}/parquet/{table_name}.parquet')
 
     # Store DataFrame in HDF5 format
-    data.to_hdf(f'{data_path}/hdf5/{table_name}-*.hdf',
-                '/data',
-                mode='w',
-                complib='blosc',
-                complevel=9,
-                min_itemsize=75)
+    # data.to_hdf(f'{data_path}/hdf5/{table_name}-*.hdf',
+    #             '/data',
+    #             mode='w',
+    #             complib='blosc',
+    #             complevel=9,
+    #             min_itemsize=75)
