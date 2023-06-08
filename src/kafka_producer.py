768b57b768f011b1ee6b6e2bb35c261abe568c70
Ermin Omeragic <eo3031@student.uni-lj.si>
2023-06-08 13:46:36 +0200

    Also added changed kafka producer script

--- src/kafka_producer.py
+++ src/kafka_producer.py
@@ -3,7 +3,12 @@ import pandas as pd
 import os
 import json
 import time
+import sys
+import inspect
 
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
 
 def on_send_success(record_metadata):
     print(record_metadata.topic)
@@ -18,14 +23,9 @@ def on_send_error(excp):
 if __name__ == '__main__':
     producer = KafkaProducer(bootstrap_servers='localhost:9092', client_id='streamz-producer', buffer_memory=134217728,
                              value_serializer=lambda m: json.dumps(m).encode('ascii'))
-    files = os.listdir('../data/parquet/PARKING_VIOLATION_ISSUED.parquet')
-    codes = pd.read_csv('../data/fine_codes.csv', index_col=False)
+    files = os.listdir('./data/parquet/AUGMENTED_PARKING_VIOLATION_ISSUED.parquet')
     for file in files:
-        data = pd.read_parquet('../data/parquet/PARKING_VIOLATION_ISSUED.parquet' + '/' + file)
-        data['Violation Code'] = data['Violation Code'].astype('int64')
-        codes['Violation Code'] = codes['Violation Code'].astype('int64')
-        data = data.join(codes.set_index('Violation Code'), on='Violation Code', how="left", lsuffix='_left',
-                         rsuffix='_right')
+        data = pd.read_parquet('./data/parquet/AUGMENTED_PARKING_VIOLATION_ISSUED.parquet' + '/' + file)
         for idx, (i, datapoint) in enumerate(data.iterrows()):
             producer.send('streamz-topic', datapoint.to_json()).add_callback(on_send_success).add_errback(on_send_error)
             time.sleep(0.01)
