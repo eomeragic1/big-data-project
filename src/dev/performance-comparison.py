ca657ffe04813cd3a58c1931b1e8fe4dc914f529
lukazontar <97096253+lukazontar@users.noreply.github.com>
2023-06-12 00:10:35 +0200

    Final commit

--- src/dev/performance-comparison.py
+++ src/dev/performance-comparison.py
@@ -4,18 +4,19 @@ import seaborn as sns
 
 data = pd.read_parquet(f'data/parquet/PROCESSED_TABLE_METADATA.parquet')
 data['Execution time (s) / row'] = data['Execution time'] / data['Row count ']
-data['Peak memory usage [MB] / row'] = data['Peak memory usage'] / data['Row count ']
+data['Peak memory usage [MB]'] = data['Peak memory usage']
 data = data.sort_values(by=['Execution time (s) / row'])
 
 fig, ax = plt.subplots(nrows=2, ncols=2)
+plt.suptitle('Performance Comparison', fontsize=24)
+
 fig.set_figwidth(12)
 fig.set_figheight(8)
 sns.barplot(data, x='Execution time (s) / row', y='File mode', ax=ax[0][0])
 sns.barplot(data, x='Execution time (s) / row', y='Processing mode', ax=ax[0][1])
 
-sns.barplot(data, x='Peak memory usage [MB] / row', y='File mode', ax=ax[1][0])
-sns.barplot(data, x='Peak memory usage [MB] / row', y='Processing mode', ax=ax[1][1])
-
+sns.barplot(data, x='Peak memory usage [MB]', y='File mode', ax=ax[1][0])
+sns.barplot(data, x='Peak memory usage [MB]', y='Processing mode', ax=ax[1][1])
 plt.show()
 
 
@@ -31,7 +32,7 @@ plt.xlim((0, 100))
 plt.legend(fontsize="20")
 plt.xlabel(xlabel='Completeness [%]', fontsize=20)
 plt.xticks(fontsize="16")
-
+plt.title('Table Completeness by Column', fontsize=24)
 plt.show()
 
 data = pd.read_parquet(f'data/parquet/PROCESSED_COUNT_BY_DATE.parquet')
@@ -39,4 +40,5 @@ data['Year'] = data['Date'].dt.strftime('%Y').astype(int)
 
 sns.barplot(data[data['Year'] >= 2013], x='Year', y='Count', hue='Table Name')
 plt.yscale('log')
+plt.title('Row Count by Year', fontsize=24)
 plt.show()
